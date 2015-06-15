c8e11ae1de411d3eeebcaa87a2c601f9923955c4
Wagner Palacio <palaciowagner@gmail.com>
2015-06-15 01:18:55 -0300

    Adicionando feature de seguidores

--- app/views.py
+++ app/views.py
@@ -25,7 +25,7 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
-						   
+
 						   
 ##Load User function						   
 @lm.user_loader
@@ -61,6 +61,8 @@ def after_login(resp):
         nickname = User.make_unique_nickname(nickname)
         user = User(nickname = nickname, email = resp.email)
         db.session.add(user)
+        db.session.commit()
+        db.session.add(user.follow(user))
         db.session.commit()
     remember_me = False
     if 'remember_me' in session:
@@ -116,6 +118,43 @@ def edit():
         form.about_me.data = g.user.about_me
     return render_template('edit.html', form=form)
 
+@app.route('/follow/<nickname>')
+@login_required
+def follow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t follow yourself!')
+        return redirect(url_for('user', nickname=nickname))
+    u = g.user.follow(user)
+    if u is None:
+        flash('Cannot follow ' + nickname + '.')
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You are now following ' + nickname + '!')
+    return redirect(url_for('user', nickname=nickname))
+
+@app.route('/unfollow/<nickname>')
+@login_required
+def unfollow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t unfollow yourself!')
+        return redirect(url_for('user', nickname=nickname))
+    u = g.user.unfollow(user)
+    if u is None:
+        flash('Cannot unfollow ' + nickname + '.')
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You have stopped following ' + nickname + '.')
+    return redirect(url_for('user', nickname=nickname))
 
 #####ERROR HANDLERS######
 @app.errorhandler(404)
